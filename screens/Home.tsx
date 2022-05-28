1873ecb3b725deb778bc0fed9d5d151757d9fc4b
Guilherme Ytalo <gydaytalo@gmail.com>
2022-05-27 22:35:33 -0300

    add apollo client and removed grapqhl

---
+++ screens/Home.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Home = ()  => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 3,
+    backgroundColor: 'red',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
+
+
+export default Home;
